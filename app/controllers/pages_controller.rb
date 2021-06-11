eb1d6f71ea4ecafdef0e7181ecfb5206c60e6e14
Shazeen15 <shazzy.sterlin@gmail.com>
2021-06-11 11:32:06 -0700

    set up new

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+    def index
+        @pages = Page.all
+    end
+
+    def show
+        @page = Page.find(params[:id])
+    end
+
+    def new
+        @page = Page.new
+    end
+    
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_005427) do
+ActiveRecord::Schema.define(version: 2021_06_05_021428) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
